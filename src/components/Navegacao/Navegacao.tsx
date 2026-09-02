0c287416c2bd0c3e33295ab66c29940bbc7d227a
TAUANE VITÓRIA SILVA SOUZA <tauane.souza@portalsesisp.org.br>
2026-09-02 08:30:58 -0300

    Sprint4 - Atualizando logo Infotech

--- src/components/Navegacao/Navegacao.tsx
+++ src/components/Navegacao/Navegacao.tsx
@@ -1,6 +1,7 @@
 import { useState, type JSX } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import AuthRequests from "../../fetch/AuthRequests";
+import logoInfotech from "../../assets/logoInfotech.png";
 
 function Navegacao(): JSX.Element {
     const [menuAberto, setMenuAberto] = useState(false);
@@ -80,7 +81,7 @@ function Navegacao(): JSX.Element {
                     className="flex items-center gap-2"
                 >
                     <div className="flex h-9 w-9 items-center justify-center rounded-lg bg-teal-400 text-sm font-black text-black shadow-sm">
-                        IT
+                         <img src={logoInfotech} alt="InfoTech Logo" className="h-10 w-auto" />
                     </div>
 
                     <span className="text-xl font-bold tracking-tight text-white">

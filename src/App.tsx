e7cf589fc85476733ceda004bc880a6f31ef5c52
TAUANE VITÓRIA SILVA SOUZA <tauane.souza@portalsesisp.org.br>
2026-09-02 09:09:17 -0300

    Atualizando PLogin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PHome from './pages/PHome/PHome'
+import PLogin from './pages/PLogin/PLogin'
+
 
 function App() {
 
@@ -10,6 +11,9 @@ function App() {
 
       <Routes>
         <Route path='/' element={<PHome />} />
+        <Route path='/login' element={<PLogin />} />
+        <Route
+        />
       </Routes>
     </BrowserRouter>
   )

047a7bae856568292d745c06bdcfc88f0852e2b8
TAUANE VITÓRIA SILVA SOUZA <tauane.souza@portalsesisp.org.br>
2026-09-02 14:25:26 -0300

    Sprint6 - feat: Atualizando app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import PHome from './pages/PHome/PHome'
 import PLogin from './pages/PLogin/PLogin'
 import PListagemProduto from './pages/PListagemProduto/PListagemProduto'
 import PListagemMovimentacao from './pages/PListagemMovimentacao/PListagemMovimentacao'
 import PListagemCategoria from './pages/PListagemCategoria/PListagemCategoria'
+
+import PDetalhesProduto from './pages/PListagemProduto/PListagemProduto'
+import PDetalhesCategoria from './pages/PListagemCategoria/PListagemCategoria'
+import PDetalhesMovimentacao from './pages/PListagemMovimentacao/PListagemMovimentacao'
+
 // import ProtectedRoute from './components/Rotas/ProtectedRoutes'
 
 function App() {
@@ -13,21 +20,52 @@ function App() {
     <BrowserRouter>
 
       <Routes>
-        <Route path='/' element={<PHome />} />
-        <Route path='/login' element={<PLogin />} />
+
+        <Route
+          path='/'
+          element={<PHome />}
+        />
+
+        <Route
+          path='/login'
+          element={<PLogin />}
+        />
+
+        {/* PRODUTOS */}
         <Route
           path='/lista/produtos'
           element={<PListagemProduto />}
         />
+
+        <Route
+          path='/detalhes/produto/:id'
+          element={<PDetalhesProduto />}
+        />
+
+        {/* MOVIMENTAÇÕES */}
         <Route
           path='/lista/movimentacoes'
           element={<PListagemMovimentacao />}
         />
+
+        <Route
+          path='/detalhes/movimentacao/:id'
+          element={<PDetalhesMovimentacao />}
+        />
+
+        {/* CATEGORIAS */}
         <Route
           path='/lista/categorias'
           element={<PListagemCategoria />}
         />
+
+        <Route
+          path='/detalhes/categoria/:id'
+          element={<PDetalhesCategoria />}
+        />
+
       </Routes>
+
     </BrowserRouter>
   )
 }
